7d4863ce8ae722039a9f4d9de74e76d4148ff139
Brady Adams <brady@netzero.net>
2017-10-23 17:53:21 -0600

    wip

---
+++ dungeons-and-dragons/src/dnd/Merchantile.java
@@ -0,0 +1,10 @@
+package dnd;
+
+import java.util.List;
+
+public class Merchantile {
+	List<Weapon> Weapons;
+	List<Armor> Armors;
+	
+	
+}
--- dungeons-and-dragons/src/dnd/Weapon.java
+++ dungeons-and-dragons/src/dnd/Weapon.java
@@ -1,10 +1,13 @@
 package dnd;
 
 public class Weapon {
+	private String type;
 	private float cost;
 	private int damage;
 	
-	public Weapon() {
-		
+	public Weapon(String type, float cost, int damage) {
+		this.type = type;
+		this.cost = cost;
+		this.damage = damage;
 	}
 }
