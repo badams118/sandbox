5e9819bdf31593e2ed75fc5c860cfc6c0df74b11
Brady Adams <brady@netzero.net>
2017-10-24 15:47:20 -0600

    Adding monster class

--- dungeons-and-dragons/src/dnd/Monster.java
+++ dungeons-and-dragons/src/dnd/Monster.java
@@ -1,5 +1,28 @@
 package dnd;
 
-public class Monster {
+import java.util.Random;
 
+import dnd.Weapon.TargetSize;
+
+public class Monster {
+	private String type;
+	private CharacterClass characterClass;
+	private int level;
+	private int armorClass;
+	private int hitPoints;
+	private int damageLow;
+	private int damageHigh;
+	
+	public Monster(String type, CharacterClass characterClass, int level, int armorClass, int hitPoints, int damageLow, int damageHigh) {
+		this.type = type;
+		this.characterClass = characterClass;
+		this.armorClass = armorClass;
+		this.hitPoints = hitPoints;
+		this.damageLow = damageLow;
+		this.damageHigh = damageHigh;
+	}
+	
+	public void strikeMelee(Object target) {
+		int damage = new Random().nextInt(damageHigh - damageLow + 1) + damageLow;
+	}
 }
