9b4ef52ddc6ffef898af682a252b39c84b47fd84
Brady Adams <brady@netzero.net>
2017-10-30 14:16:01 -0600

    created combatmatrix class

---
+++ dungeons-and-dragons/src/dnd/CombatMatrix.java
@@ -0,0 +1,11 @@
+package dnd;
+
+public class CombatMatrix {
+	public static boolean isHit(CharacterClass attackerCharacterClass, int attackerLevel, int targetArmorClass) {
+		boolean isHit = false;
+		
+		
+		
+		return isHit;
+	}
+}
