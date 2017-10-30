583d56b0954feed854a0a5a2061da18c5f301715
Brady Adams <brady@netzero.net>
2017-10-30 13:53:17 -0600

    wip

--- dungeons-and-dragons/src/dnd/Driver.java
+++ dungeons-and-dragons/src/dnd/Driver.java
@@ -1,5 +1,7 @@
 package dnd;
 
+import java.util.Random;
+
 public class Driver {
 
 	public static void main(String[] args) {
@@ -13,10 +15,12 @@ public class Driver {
 		Monster skeleton = new Monster("skeleton", 7, 1, 0, 1, 6, CharacterClass.FIGHTER, 1, 12);
 		System.out.println(skeleton.toString());
 		
-		while (true) {
+		if (new Random().nextBoolean()) {
 			System.out.println(joe.getName() + " strikes " + skeleton.getType() + " for " + 
 					Integer.toString(joe.strikeMelee(skeleton)) + " damage.");
 			System.out.println(skeleton.getType() + " hit points: " + Integer.toString(skeleton.getHitPoints()));
+		}
+		while (true) {	
 			if (skeleton.getHitPoints() <= 0) {
 				System.out.println(skeleton.getType() + " has died.");
 				break;
@@ -25,10 +29,14 @@ public class Driver {
 			System.out.println(skeleton.getType() + " strikes " + joe.getName() + " for " + 
 					Integer.toString(skeleton.strikeMelee(joe)) + " damage.");
 			System.out.println(joe.getName() + " hit points: " + Integer.toString(joe.getHitPoints()));
-			if (joe.getHitPoints() <= 0) {
+			if (joe.getHitPoints() <= 0 || skeleton.getHitPoints() <= 0) {
 				System.out.println(joe.getName() + " has died.");
 				break;
 			}
+			
+			System.out.println(joe.getName() + " strikes " + skeleton.getType() + " for " + 
+					Integer.toString(joe.strikeMelee(skeleton)) + " damage.");
+			System.out.println(skeleton.getType() + " hit points: " + Integer.toString(skeleton.getHitPoints()));
 		}
 		
 	}
